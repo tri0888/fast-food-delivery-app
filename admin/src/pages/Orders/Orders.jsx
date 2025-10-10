a9f9907978226d5a31dc658264f7ca355f4ccc40
NguyenTanDai_3122411035 <70790046+gamethu@users.noreply.github.com>
2025-10-10 08:24:02 +0700

    MODIFY:      1. fix docker admin

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import { toast } from 'react-toastify';
 import { useEffect } from 'react';
-import { assets } from './../../../../frontend/src/assets/assets';
+import { assets } from './../../assets/assets';
 import ConfirmDialog from '../../components/ConfirmDialog/ConfirmDialog';
 
 const Orders = ({url}) => {
